01a177ab9d050a6aa3179903b825b79b26818920
sirozu <rychen123@yandex.ru>
2024-03-29 21:57:16 +0300

    setting console project

--- UI/CryptoMonitor.ConsoleUI/Program.cs
+++ UI/CryptoMonitor.ConsoleUI/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CryptoMonitor.ConsoleUI
+{
+    class Program
+    {
+        private static IHost __Hosting;
+
+        public static IHost Hosting => __Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host
+                .CreateDefaultBuilder(args)
+                .ConfigureServices(ConfigureServices);
+        }
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            
+        }
+
+        static async Task Main(string[] args)
+        {
+            using var host = Hosting;
+            await host.StartAsync();
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+
+            await host.StopAsync();
+        }
+    }
+}
