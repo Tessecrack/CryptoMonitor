38da8eed54842d12edf247db9c7e6e1bcab16250
sirozu <rychen123@yandex.ru>
2024-03-30 21:58:20 +0300

    named entity repository

---
+++ Data/CryptoMonitor.DAL/Repositories/DbNamedRepository.cs
@@ -0,0 +1,13 @@
+﻿using CryptoMonitor.DAL.Context;
+using CryptoMonitor.DAL.Entities.Base;
+
+namespace CryptoMonitor.DAL.Repositories
+{
+    public class DbNamedRepository<T> : DbRepository<T> where T : Entity, new()
+    {
+        public DbNamedRepository(DataDB db) : base(db)
+        {
+
+        }
+    }
+}
