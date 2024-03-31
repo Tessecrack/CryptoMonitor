2ad9f34d39ce9de7fc21dd3edee22db07218287b
sirozu <rychen123@yandex.ru>
2024-03-31 22:06:19 +0300

    added controller WebAPI for access to repository

---
+++ Services/CryptoMonitor.API/Controllers/DataSourcesController.cs
@@ -0,0 +1,24 @@
+﻿using CryptoMonitor.DAL.Entities;
+using CryptoMonitor.Interfaces.Base.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoMonitor.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataSourcesController : ControllerBase
+    {
+        private readonly IRepository<DataSource> _repository;
+
+        public DataSourcesController(IRepository<DataSource> repository)
+        {
+            this._repository = repository;
+        }
+
+        [HttpGet("count")]
+        public async Task<IActionResult> GetItemsCount()
+        {
+            return Ok(await _repository.GetCountAsync());
+        }
+    }
+}
