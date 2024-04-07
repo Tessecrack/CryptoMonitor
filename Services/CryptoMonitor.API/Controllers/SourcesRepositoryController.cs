7496856eb589b01b3f5e11993d56bd3dc60caf5c
sirozu <rychen123@yandex.ru>
2024-04-07 18:18:07 +0300

    added data sources controller

---
+++ Services/CryptoMonitor.API/Controllers/SourcesRepositoryController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CryptoMonitor.API.Controllers.Base;
+using CryptoMonitor.DAL.Entities;
+using CryptoMonitor.Domain.Base;
+using CryptoMonitor.Interfaces.Base.Repositories;
+
+namespace CryptoMonitor.API.Controllers
+{
+	public class SourcesRepositoryController : MapperEntityController<DataSourceInfo, DataSource>
+	{
+		public SourcesRepositoryController(IRepository<DataSource> repository, IMapper mapper) 
+			: base(repository, mapper)
+		{
+
+		}
+	}
+}
