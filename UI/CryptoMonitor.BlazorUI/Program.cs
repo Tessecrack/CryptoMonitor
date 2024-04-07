1d1cc176f565722052eaadaf9016e5f38ce97f24
sirozu <rychen123@yandex.ru>
2024-04-07 18:54:29 +0300

    added extension method AddApi

--- UI/CryptoMonitor.BlazorUI/Program.cs
+++ UI/CryptoMonitor.BlazorUI/Program.cs
@@ -1,4 +1,8 @@
 using CryptoMonitor.BlazorUI;
+using CryptoMonitor.BlazorUI.Infrastructure.Extensions;
+using CryptoMonitor.Domain.Base;
+using CryptoMonitor.Interfaces.Base.Repositories;
+using CryptoMonitor.WebAPIClients.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +10,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var services = builder.Services;
+services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+services.AddApi<IRepository<DataSourceInfo>, WebRepository<DataSourceInfo>>("api/SourcesRepository");
 
 await builder.Build().RunAsync();
