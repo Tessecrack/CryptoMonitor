2872db44870c0bb127e51d29368b85c7d41da650
sirozu <rychen123@yandex.ru>
2024-04-07 12:35:05 +0300

    business logic data models

---
+++ Common/CryptoMonitor.Domain.Base/DataSourceInfo.cs
@@ -0,0 +1,24 @@
+﻿using CryptoMonitor.Interfaces.Base.Entities;
+
+namespace CryptoMonitor.Domain.Base
+{
+	public class DataSourceInfo : INamedEntity
+	{
+		public string Name { get; set; }
+
+		public int Id { get; set; }
+
+		public string Description { get; set; }
+	}
+
+	public class DataValueInfo : IEntity
+	{
+		public int Id { get; set; }
+
+		public DateTimeOffset Time { get; set; }
+
+		public string Value { get; set; }
+
+		public bool IsFaulted { get; set; }
+	}
+}
